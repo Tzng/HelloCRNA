348c01681d91ae2e2932d166622bc98d0a4c3df6
allahbin <569693942@qq.com>
2018-05-26 21:31:47 +0800

    设置头部通用样式，在最下面

--- App.js
+++ App.js
@@ -123,6 +123,15 @@ const RootStack = createStackNavigator(
     },
     {
         initialRouteName: 'Home',
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                fontWeight: 'bold',
+            },
+        }
     }
 );
 
